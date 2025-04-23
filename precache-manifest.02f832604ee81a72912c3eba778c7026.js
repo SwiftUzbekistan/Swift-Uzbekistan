37592888ac9c519668311fc9e1306855f47ba197
Muhammad Khodjaev <muhammadspc@Muhammads-MacBook-Air.local>
2025-04-24 02:50:34 +0500

    Updates

---
+++ precache-manifest.02f832604ee81a72912c3eba778c7026.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab7b0a6423275aa083935755aa82188",
+    "url": "/swift-uzbekistan/index.html"
+  },
+  {
+    "revision": "fab962eb8d27296cb0b7",
+    "url": "/swift-uzbekistan/static/css/main.a45fe09f.chunk.css"
+  },
+  {
+    "revision": "0a4e550e505fff56e3c3",
+    "url": "/swift-uzbekistan/static/js/2.ab3c5c79.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/swift-uzbekistan/static/js/2.ab3c5c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7ea586177b74056230",
+    "url": "/swift-uzbekistan/static/js/3.8f9afd11.chunk.js"
+  },
+  {
+    "revision": "fab962eb8d27296cb0b7",
+    "url": "/swift-uzbekistan/static/js/main.f67b9596.chunk.js"
+  },
+  {
+    "revision": "fd6579fb3ab042f11799",
+    "url": "/swift-uzbekistan/static/js/runtime-main.0324fc9c.js"
+  },
+  {
+    "revision": "da81c9c97f3b02541c634b5e712eeaa1",
+    "url": "/swift-uzbekistan/static/media/Swift_logo_color.da81c9c9.svg"
+  }
+]);
